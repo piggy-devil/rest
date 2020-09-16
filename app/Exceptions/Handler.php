ccd2987ccf8107f2abdc91f2a214e7391c4ce2b9
piggy-devil <muupaza@gmail.com>
2020-09-16 15:04:30 +0700

    Validation Errors as a JSON Response

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,10 +2,17 @@
 
 namespace App\Exceptions;
 
+use Exception;
+use Throwable;
+use App\Traits\ApiResponser;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
     /**
      * A list of the exception types that are not reported.
      *
@@ -33,5 +40,68 @@ class Handler extends ExceptionHandler
     public function register()
     {
         //
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $e
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Throwable
+     */
+    public function render($request, Throwable $e)
+    {
+        // if (method_exists($e, 'render') && $response = $e->render($request)) {
+        //     return Router::toResponse($request, $response);
+        // } elseif ($e instanceof Responsable) {
+        //     return $e->toResponse($request);
+        // }
+
+        // $e = $this->prepareException($this->mapException($e));
+
+        // foreach ($this->renderCallbacks as $renderCallback) {
+        //     if (is_a($e, $this->firstClosureParameterType($renderCallback))) {
+        //         $response = $renderCallback($e, $request);
+
+        //         if (! is_null($response)) {
+        //             return $response;
+        //         }
+        //     }
+        // }
+
+        if ($e instanceof HttpResponseException) {
+            return $e->getResponse();
+        } elseif ($e instanceof AuthenticationException) {
+            return $this->unauthenticated($request, $e);
+        } elseif ($e instanceof ValidationException) {
+            return $this->convertValidationExceptionToResponse($e, $request);
+        }
+
+        return $request->expectsJson()
+                    ? $this->prepareJsonResponse($request, $e)
+                    : $this->prepareResponse($request, $e);
+    }
+
+        /**
+     * Create a response object from the given validation exception.
+     *
+     * @param  \Illuminate\Validation\ValidationException  $e
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function convertValidationExceptionToResponse(ValidationException $e, $request)
+    {
+        // if ($e->response) {
+        //     return $e->response;
+        // }
+
+        // return $request->expectsJson()
+        //             ? $this->invalidJson($request, $e)
+        //             : $this->invalid($request, $e);
+        $errors = $e->validator->errors()->getMessages();
+
+        return $this->errorResponse($errors, 422);
     }
 }
