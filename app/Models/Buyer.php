9d61246ce53fa9f2aa04d73a5cb533ca63e5b001
piggy-devil <muupaza@gmail.com>
2020-09-16 16:50:30 +0700

    Buyer Using Laravel Global Scopes

--- app/Models/Buyer.php
+++ app/Models/Buyer.php
@@ -3,12 +3,18 @@
 namespace App\Models;
 
 use App\Models\Transaction;
+use App\Scopes\BuyerScope;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Buyer extends User
 {
-
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope(new BuyerScope);
+    }
+    
     public function transactions()
     {
         return $this->hasMany(Transaction::class);
