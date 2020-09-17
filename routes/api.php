e6acefca9985e0398afce8f4f0c666673b353cdf
piggy-devil <muupaza@gmail.com>
2020-09-17 16:05:59 +0700

    SellerTransaction, SellerCategory, SellerBuyer, SellerProduct(Index,Store,Update,Destroy)

--- routes/api.php
+++ routes/api.php
@@ -9,12 +9,16 @@ use App\Http\Controllers\Product\ProductController;
 use App\Http\Controllers\Buyer\BuyerSellerController;
 use App\Http\Controllers\Category\CategoryController;
 use App\Http\Controllers\Buyer\BuyerProductController;
+use App\Http\Controllers\Seller\SellerBuyerController;
 use App\Http\Controllers\Buyer\BuyerCategoryController;
+use App\Http\Controllers\Seller\SellerProductController;
+use App\Http\Controllers\Seller\SellerCategoryController;
 use App\Http\Controllers\Buyer\BuyerTransactionController;
 use App\Http\Controllers\Category\CategoryBuyerController;
 use App\Http\Controllers\Category\CategorySellerController;
 use App\Http\Controllers\Transaction\TransactionController;
 use App\Http\Controllers\Category\CategoryProductController;
+use App\Http\Controllers\Seller\SellerTransactionController;
 use App\Http\Controllers\Category\CategoryTransactionController;
 use App\Http\Controllers\Transaction\TransactionSellerController;
 use App\Http\Controllers\Transaction\TransactionCategoryController;
@@ -62,6 +66,10 @@ Route::resource('products', ProductController::class)->only(['index', 'show']);
  * sellers
  * */
 Route::resource('sellers', SellerController::class)->only(['index', 'show']);
+Route::resource('sellers.transactions', SellerTransactionController::class)->only(['index']);
+Route::resource('sellers.categories', SellerCategoryController::class)->only(['index']);
+Route::resource('sellers.buyers', SellerBuyerController::class)->only(['index']);
+Route::resource('sellers.products', SellerProductController::class)->except(['show', 'create', 'edit']);
 
 /**
  * transactions
