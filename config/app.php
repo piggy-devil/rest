8c2d3e4daaa9ced78729ba5e5c2bf9ef2d33b4cf
piggy-devil <muupaza@gmail.com>
2020-09-21 14:49:41 +0700

    composer require spatie/laravel-fractal

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
 
+         Spatie\Fractal\FractalServiceProvider::class,
         /*
          * Application Service Providers...
          */
