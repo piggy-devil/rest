c042a27ef224649733190a4645c833487bf53104
piggy-devil <muupaza@gmail.com>
2020-09-21 16:34:35 +0700

    Sorting

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -25,6 +25,7 @@ trait ApiResponser
 
         $transformer = $collection->first()->transformer;
 
+        $collection = $this->sortData($collection, $transformer);
         $collection = $this->transformData($collection, $transformer);
 
         return $this->successResponse($collection, $code);
@@ -42,6 +43,16 @@ trait ApiResponser
     protected function showMessage($message, $code = 200)
     {
         return $this->successResponse(['data' => $message], $code);
+    }
+
+    protected function sortData(Collection $collection, $transformer)
+    {
+        if (request()->has('sort_by')) {
+            $attribute = $transformer::originalAttribute(request()->sort_by);
+
+            $collection = $collection->sortBy->{$attribute};
+        }
+        return $collection;
     }
 
     protected function transformData($data, $transformer)
